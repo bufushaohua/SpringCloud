ae787f9e93a406513b70efdec0e6e236d22f83d2
Mr.Cyc <40974355+bufushaohua@users.noreply.github.com>
2020-08-23 23:20:02 +0800

    2.支付模块构建

---
+++ cloud_provider-payment8001/src/main/java/com/cyc/springcloud/controller/PaymentController.java
@@ -0,0 +1,45 @@
+package com.cyc.springcloud.controller;
+
+import com.cyc.springcloud.entities.CommonResult;
+import com.cyc.springcloud.entities.Payment;
+import com.cyc.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Resource
+    public PaymentService paymentService;
+
+    //浏览器对post请求不支持，可以使用postman软件进行测试
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(Payment payment){
+        int result = paymentService.create(payment);
+        log.info("****插入结果：" + result);
+
+        if(result>0){
+            return new CommonResult(200,"插入数据库成功",result);
+        }else{
+            return new CommonResult(444,"插入数据库失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/query/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") long id){
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("*****查询结果" + payment);
+
+        if(payment != null){
+            return new CommonResult(200,"查询数据成功", payment);
+        }else
+        {
+            return new CommonResult(444,"没有对应记录，查询ID" + id,null);
+        }
+
+    }
+
+}
---
+++ cloud_provider-payment8001/src/main/java/com/cyc/springcloud/service/PaymentServiceImpl.java
@@ -0,0 +1,24 @@
+package com.cyc.springcloud.service;
+
+import com.cyc.springcloud.dao.PaymentDao;
+import com.cyc.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment){
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id){
+        return paymentDao.getPaymentById(id);
+    }
+
+}
